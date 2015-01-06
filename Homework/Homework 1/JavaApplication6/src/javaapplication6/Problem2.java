421eb07a7d11bce8b094f39d2d4ea7b616ac946b
treardon17 <tdr.reardon@gmail.com>
2015-01-06 14:33:50 -0800

    Problem 2 completed

---
+++ Homework/Homework 1/JavaApplication6/src/javaapplication6/Problem2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author tylerreardon
+ */
+public class Problem2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { 
+        if (args.length > 0)
+            System.out.println(args[0]);
+        if (args.length > 1)
+            System.out.println(args[1]);
+        if (args.length > 2)
+            System.out.println(args[2]);
+    }
+    
+}
