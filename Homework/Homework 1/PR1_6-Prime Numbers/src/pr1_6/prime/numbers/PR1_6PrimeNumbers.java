418bff46f0056e30e010985d7780b3d7b1fd0016
treardon17 <tdr.reardon@gmail.com>
2015-01-05 19:46:01 -0800

    Added comments

--- Homework/Homework 1/PR1_6-Prime Numbers/src/pr1_6/prime/numbers/PR1_6PrimeNumbers.java
+++ Homework/Homework 1/PR1_6-Prime Numbers/src/pr1_6/prime/numbers/PR1_6PrimeNumbers.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package pr1_6.prime.numbers;
-
+import java.util.Scanner;
 /**
  *
  * @author tylerreardon
@@ -15,16 +15,18 @@ public class PR1_6PrimeNumbers {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        for (int i = 2; i<100; i++){
-            boolean prime = true;
+        System.out.println("Max Number: ");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt(); //read in max number from user
+        for (int i = 2; i<num; i++){
+            boolean prime = true; //default to setting each number to prime
             for (int j = 2; j<i-1; j++){
-                if ((i%j) == 0){
-                    prime = false;
+                if ((i%j) == 0){ //if the number is divisible by more than just itself and 1...
+                    prime = false; //set prime to false
                 }
             }
-            if (prime){
-                System.out.printf("%d\n", i);
+            if (prime){ //if the number was only divisible by itself and 1, it is prime
+                System.out.printf("%d\n", i); //output the prime number
             }
         }
     }
