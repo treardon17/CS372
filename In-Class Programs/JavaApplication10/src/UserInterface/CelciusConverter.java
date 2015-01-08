ad7b98aba847aed25d53bf74d926edc03d8b77a8
treardon17 <tdr.reardon@gmail.com>
2015-01-08 14:49:42 -0800

    Celcius Converter

---
+++ In-Class Programs/JavaApplication10/src/UserInterface/CelciusConverter.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UserInterface;
+
+/**
+ *
+ * @author tylerreardon
+ */
+public class CelciusConverter {
+   private double currentCelsius;
+
+   public CelciusConverter(double c) {
+      setCelsius(c);
+   }
+
+   public double getCelsius() { return currentCelsius; }
+   public void setCelsius(double c) { currentCelsius = c; }
+
+   public double toFehrenheit() {
+      return currentCelsius * (9/5.0) + 32;
+   }
+}
+
